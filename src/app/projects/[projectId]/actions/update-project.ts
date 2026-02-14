b97371a74f4b0e3c8fd3a7e907f4a41ce08fcc4a
ihym <b.diakomanolis@gmail.com>
2026-02-14 17:34:34 +0200

    Adjust actions to support lastUpdated

--- src/app/projects/[projectId]/actions/update-project.ts
+++ src/app/projects/[projectId]/actions/update-project.ts
@@ -5,17 +5,30 @@ import { Project } from '@prisma/client'
 import { revalidatePath } from 'next/cache'
 
 export const updateProject = async (project: Project) => {
-  const updatedProject = await prisma.project.update({
-    where: { id: project.id },
+  // update the project if the current lastUpdated in the db is less than the client's lastUpdated
+  // avoids race conditions where multiple requests are sent at the same time
+  // the last client update always wins
+  const { count } = await prisma.project.updateMany({
+    where: {
+      id: project.id,
+      lastUpdated: { lt: project.lastUpdated },
+    },
     data: {
+      lastUpdated: project.lastUpdated,
       title: project.title,
       subhead: project.subhead,
       description: project.description,
     },
   })
 
-  revalidatePath('/projects')
-  revalidatePath(`/projects/${project.id}`)
+  if (count > 0) {
+    revalidatePath('/projects')
+    revalidatePath(`/projects/${project.id}`)
+  }
+
+  const updatedProject = await prisma.project.findUniqueOrThrow({
+    where: { id: project.id },
+  })
 
   return updatedProject
 }
--- src/app/projects/actions/create-project.ts
+++ src/app/projects/actions/create-project.ts
@@ -4,7 +4,9 @@ import { prisma } from '@/prisma/prisma'
 import { revalidatePath } from 'next/cache'
 
 export const createEmptyProject = async () => {
-  const project = await prisma.project.create({})
+  const project = await prisma.project.create({
+    data: { lastUpdated: new Date() },
+  })
   revalidatePath('/projects')
 
   return project
